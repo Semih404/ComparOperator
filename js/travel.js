861e08a9ea5f5e744f797d2d45646443e7d8458d
TaherKammoun <119849223+TaherKammoun@users.noreply.github.com>
2023-03-13 10:08:38 +0100

    Update travel.js

--- js/travel.js
+++ js/travel.js
@@ -32,3 +32,28 @@ videoBtn.forEach (slide =>{
         document.querySelector("#video-slider").src = videoSrc
     })
 })
+
+let dots = document.querySelectorAll(".dots span");
+        let images = ["./images/tunis.jpeg" , "./images/tunis-1.jpeg", "images/tunis-2.jpeg" , "images/tunis-3.jpeg" , "images/tunis-4.jpeg" , "images/tunis-5.jpeg" , "images/tunis-6.jpeg" ];
+        let myimage = document.querySelector("#image");
+        let righticon = document.querySelector(".right");
+        let lefticon = document.querySelector(".left");
+
+
+        let count = 0;
+        function next(a) {
+            slide(count +=a);
+        }
+        function prev (a) {
+            slide(count +=a);
+        }
+
+        function slide (e) {
+            count = e;
+            if(count > images.length -1) count = 0;
+            if(count < 0) count = images.length -1;
+            
+            dots.forEach((n) => n.classList.remove("active"));
+            dots[count].classList.add("active");
+            myimage.setAttribute("src", images[count]);
+        }
